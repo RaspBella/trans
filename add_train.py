02d4834b04dae7842ee1c98721aeac13e61bc290
Bella Campbell <Bella.Campbell@tuta.io>
2024-12-23 12:32:36 +0000

    data entry program for transport :3

---
+++ add_train.py
@@ -0,0 +1,32 @@
+def main():
+    import json
+
+    file = open("data.json", "r")
+
+    trains = json.load(file)
+
+    file.close()
+
+    train = {
+        input('Date (yyyy-mm-dd): ') : {
+            'from' : input('From (station code): ').upper(),
+            'to' : input('To (station code): ').upper(),
+            'change' : 'direct' if not (raw := input('Change (comma seperated station code(s), default is none): ')) else raw.upper().split(','),
+            'rtt' : input('RTT (comma seperated link(s)): ').split(',')
+        }
+    }
+
+    print()
+    print(train)
+
+    if 'y' in input('Does this look right?: '):
+        trains |= train
+
+        file = open("data.json", "w")
+
+        json.dump(trains, file)
+
+        file.close()
+
+if __name__ == '__main__':
+    main()
