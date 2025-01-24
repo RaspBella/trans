1c5c82f5304c806276c8e159fb04ed4c7678c803
Bella Campbell <bella.campbell@tuta.io>
2025-01-24 02:35:33 +0000

    fetch skeleton

--- future/future.js
+++ future/future.js
@@ -1,3 +1,9 @@
 const table = document.getElementsByTagName("table")[0];
 
 const today = new Date().toISOString().substring(0, 10);
+
+fetch("../data.json")
+.then((response) => response.json())
+.then((data) => {
+})
+.catch((error) => console.error("Error loading JSON file", error));
