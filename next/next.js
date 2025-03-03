6c5d910151a00ddc0e212beea2bdd649aaf80cf6
Bella Campbell <Bella.Campbell@tuta.io>
2025-03-03 21:47:04 +0000

    made next fnuy :3

--- next/next.js
+++ next/next.js
@@ -17,80 +17,29 @@ function countdown(s, redir) {
     }, 1000);
 }
 
-fetch("../data.json")
+function isNext(s) {
+    return s === "next";
+}
+
+fetch("/trans/data.json")
 .then((response) => response.json())
 .then((data) => {
-    next = [];
+    var next = location.pathname.split("/").filter(isNext).length;
+    var stack = [];
+
+    if (location.hash === "#next") next++;
+    
+    console.log(stack);
+    console.log(next);
 
     for (const date in data) {
 	if (date >= today) {
-	    next.push(date);
+	    stack.push(date);
 	}
     }
 
-    if (next.length > 0) {
-        if (next[0] === today) {
-	    let header = document.createElement("h1");
-	    header.textContent = "Today is " + today;
-	    
-	    document.body.appendChild(header);
-    
-	    if (location.hash === "#today_is_ok") {
-	        countdown(5, "/trans/" + today);
-	    }
-    
-	    else if (location.hash === "#not_today") {
-		if (next.length > 1) {
-		    countdown(5, "/trans/" + next[1]);
-		}
-
-		else {
-		    document.body.appendChild(
-			document.createElement(
-			    "h2"
-			).appendChild(
-			    document.createTextNode(
-				"No journeys upcoming :("
-			    )
-			)
-		    );
-		    
-		    countdown(5, "/trans/");
-		}
-	    }
-    
-	    else {
-		if (next.length > 1) {
-		    let header = document.createElement("h2");
-    
-		    let start = document.createTextNode("Do you want a journey after today: [");
-		    let y = document.createElement("a");
-		    let div = document.createTextNode("|");
-		    let n = document.createElement("a");
-		    let end = document.createTextNode("]");
-    
-		    y.appendChild(document.createTextNode("y"));
-		    n.appendChild(document.createTextNode("N"));
-    
-		    y.href = "/trans/" + next[1];
-		    n.href = "/trans/" + today;
-    
-		    header.appendChild(start);
-		    header.appendChild(y);
-		    header.appendChild(div);
-		    header.appendChild(n);
-		    header.appendChild(end);
-
-		    document.body.appendChild(header);
-		}
-
-		countdown(5, "/trans/" + today);
-	    }
-	}
-
-	else {
-	    countdown(5, "/trans/" + next[0]);
-	}
+    if (stack.length >= next) {
+	countdown(5, "/trans/" + stack[next - 1]);
     }
 
     else {
