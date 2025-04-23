4e76c8a4ad4e5e855566eba3828418eacd8c3e44
Bella Campbell <Bella.Campbell@tuta.io>
2025-04-23 20:07:03 +0100

    add fprint_json to header

--- v2/json.h
+++ v2/json.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 typedef enum {
   JsonNull,
   JsonBool,
@@ -14,4 +16,5 @@ typedef struct Json Json;
 Json *new_json(JsonType, void*);
 void free_json(Json*);
 
+void fprint_json(Json*, FILE*);
 void print_json(Json*);
