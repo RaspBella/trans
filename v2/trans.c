0ed470f4e5d65c174b6b5075fcefcf256d22731f
Bella Campbell <Bella.Campbell@tuta.io>
2025-04-23 18:53:43 +0100

    Start working on reading json file

--- v2/trans.c
+++ v2/trans.c
@@ -1,7 +1,9 @@
 #include <assert.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "trans.h"
+#include "json.h"
 #include "parser/json.h"
 
 static_assert(COUNT_TRANS == 6, "Amount of forms of transport has changed");
@@ -36,6 +38,46 @@ void print_trans(const Transport *trans) {
 }
 
 void read_in_table(const char *filename) {
+  FILE *fp = fopen(filename, "r");
+
+  if (fp == NULL) {
+    return;
+  }
+
+  fseek(fp, 0, SEEK_END);
+
+  long file_size = ftell(fp);
+
+  if (file_size == 0) {
+    fclose(fp);
+
+    return;
+  }
+
+  char *str = calloc(file_size + 1, sizeof(char));
+
+  rewind(fp);
+
+  if (fread(str, sizeof(char), file_size, fp) != file_size) {
+    fprintf(stderr, "Failed to read %zu bytes from file: %s\n", file_size, filename);
+
+    fclose(fp);
+    free(str);
+
+    exit(EXIT_FAILURE);
+  }
+
+  fclose(fp);
+
+  Json *data = json_parser(filename, str);
+
+  print_json(data);
+
+  if (data) {
+    free_json(data);
+  }
+
+  free(str);
 }
 
 void write_out_table(const char *filename) {
