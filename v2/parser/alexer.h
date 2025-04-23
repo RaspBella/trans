ba11bab6b14abef84098ce05b0d899c7cbd443ce
Bella Campbell <Bella.Campbell@tuta.io>
2025-04-23 18:53:07 +0100

    Currently unused var

--- v2/parser/alexer.h
+++ v2/parser/alexer.h
@@ -92,7 +92,7 @@ typedef enum {
 } Alexer_Kind;
 
 static_assert(ALEXER_COUNT_KINDS == 7, "Amount of kinds have changed");
-static const char *alexer_kind_names[ALEXER_COUNT_KINDS] = {
+__attribute__((unused)) static const char *alexer_kind_names[ALEXER_COUNT_KINDS] = {
     [ALEXER_INVALID] = "INVALID",
     [ALEXER_END]     = "END",
     [ALEXER_INT]     = "INT",
