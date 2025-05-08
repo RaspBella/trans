f3f955d2c8de9c3622ee23049eef0e7781cfd7dc
Bella Campbell <Bella.Campbell@tuta.io>
2025-05-08 22:42:12 +0100

    Don't need transport structure

--- v2/trans.h
+++ v2/trans.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define TRANS_VERSION 2
+
 typedef enum {
   TRANS_WALK,
   TRANS_BUS,
@@ -10,21 +12,4 @@ typedef enum {
   COUNT_TRANS
 } Transport_Index;
 
-typedef struct Trans {
-  Transport_Index type;
-  char from[4];
-  char to[4];
-  char *info;
-  struct Trans *child;
-  struct Trans *next;
-} Transport;
-
 extern const char *transport_strings[COUNT_TRANS];
-
-#define DIGIT 10
-
-extern Transport *table[DIGIT][DIGIT][DIGIT][DIGIT][DIGIT][DIGIT][DIGIT][DIGIT];
-
-void print_trans(const Transport *trans);
-void read_in_table(const char *filename);
-void write_out_table(const char *filename);
