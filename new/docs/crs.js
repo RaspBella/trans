775d84f9d05db33e74f1ddcc6651f368f4ed63a7
Bella Campbell <Bella.Campbell@tuta.io>
2025-08-13 18:35:06 +0100

    emcc is a pain

---
+++ new/docs/crs.js
@@ -0,0 +1,23 @@
+import createModule from './crs.mjs';
+
+(async () => {
+  const Module = await createModule();
+
+  function crs(code) {
+    const bytes = Module.lengthBytesUTF8(code) + 1;
+
+    const input = Module._malloc(bytes);
+
+    Module.stringToUTF8(code, input, bytes);
+
+    const output = Module._crs(input);
+
+    const result = Module.UTF8ToString(output);
+
+    Module._free(input);
+
+    return result;
+  }
+
+  window.crs = crs;
+})();
