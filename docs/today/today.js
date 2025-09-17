ac7cc5560ff9c0cbb7128971be021429438be504
Bella Campbell <Bella.Campbell@tuta.io>
2025-09-17 13:23:10 +0100

    some renamey

--- docs/today/today.js
+++ docs/today/today.js
@@ -1,23 +1,23 @@
 import("../countdown.js").then(({ countdown }) => {
-  const today = new Date().toISOString().substring(0, 10);
+  const date = new Date().toISOString().substring(0, 10);
 
   fetch("../data.json")
     .then((response) => response.json())
     .then((data) => {
-      if (Object.hasOwn(data, today)) {
+      if (Object.hasOwn(data, date)) {
         let header = document.createElement("h1");
 
-        header.textContext = `journey today: ${today}`;
+        header.textContext = `journey today: ${date}`;
 
         document.body.appendChild(header);
 
-        countdown(5, `/trans/${today}`);
+        countdown(5, `/trans/${date}`);
       }
 
       else {
         let header = document.createElement("h1");
 
-        header.textContent = `none today: ${today}`;
+        header.textContent = `no journey today: ${date}`;
 
         document.body.appendChild(header);
 
