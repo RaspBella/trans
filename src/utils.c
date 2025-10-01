f912cca2d6515a8991890fd9c62035ce9963dc66
Bella Campbell <Bella.Campbell@tuta.io>
2025-10-01 20:44:08 +0100

    Create utils.c for usage func

---
+++ src/utils.c
@@ -0,0 +1,30 @@
+#include "utils.h"
+
+#include <stdio.h>
+
+void usage(const char *program) {
+  fprintf(
+    stderr,
+    "usage: %s `filename`\n"
+    "commands:\n"
+    "  exit(`n`)                                          Default arg(integer) is 0\n"
+    "  print(`date`)                                      No arg will print all in-use dates`\n"
+    "  [`date`] = `journey`                               Set a journey to a date\n"
+    "  [`date`] += `journey`                              Append a journey to a date\n"
+    "\n"
+    "  `date`:\n"
+    "    `yyyy`-`mm`-`dd`                                 ISO 8601\n"
+    "\n"
+    "  `journey`:\n"
+    "    `code`->`code` `string` `link` `journey-list`    `link` and `journey-list` are optional\n"
+    "\n"
+    "  `code`:                                            A CRS code comprised of 3 capital letters\n"
+    "\n"
+    "  `link`:\n"
+    "    - `string`\n"
+    "\n"
+    "  `journey-list`:\n"
+    "    [ `journeys` ]                                   `journeys` is a comma seperated sequence of `journey`\n",
+    program
+  );
+}
