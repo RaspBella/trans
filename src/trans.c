e18ec41aabfada1de2dbe1cd63c9ea921a07a50a
Bella Campbell <bella.campbell@tuta.io>
2025-10-07 17:11:51 +0100

    Simplify main and introduce functions for the modes

--- src/trans.c
+++ src/trans.c
@@ -11,95 +11,124 @@
 
 #define pop(argc, argv) *argv++; argc--;
 
-char *mode;
-bool write_back;
+#define INDENT 2
 
-char *file;
+char *input;
+char *output;
 
 char *line;
 
 int indent;
 
 void cleanup(void) {
-  if (file) {
-    if (root) {
-      if (write_back) {
-        if (!dump(file, indent)) {
-          fprintf(stderr, "Error writing file: `%s`\n", file);
+  if (root) {
+    if (!output) {
+      if (!dump(input, indent)) {
+        fprintf(stderr, "Error writing file: `%s`\n", input);
 
-          exit(EXIT_FAILURE);
-        }
+        exit(EXIT_FAILURE);
       }
+    }
 
-      else {
-        free_json(root);
-      }
+    else {
+      free_json(root);
     }
   }
 
   if (line) free(line);
 }
 
+void add(int argc, char **argv) {
+  if (!argc) {
+    usage("add");
+
+    exit(EXIT_FAILURE);
+  }
+
+  input = pop(argc, argv);
+
+  fprintf(stderr, "add mode\n");
+
+  if (!load(input)) {
+    fprintf(stderr, "Error reading file: `%s`\n", input);
+
+    exit(EXIT_FAILURE);
+  }
+
+  while ((line = bestlineWithHistory("trans >>> ", "trans"))) {
+    bool ret = parse(line, .indent = indent);
+
+    if (!ret) {
+      fprintf(stderr, "Couldn't parse: `%s`\n", line);
+    }
+
+    free(line);
+  }
+
+  printf("exit()\n");
+
+  exit(EXIT_SUCCESS);
+}
+
+void gen(int argc, char **argv) {
+  if (argc < 2) {
+    usage("gen");
+
+    exit(EXIT_FAILURE);
+  }
+
+  input = pop(argc, argv);
+  output = pop(argc, argv);
+
+  fprintf(stderr, "gen mode\n");
+
+  if (!load(input)) {
+    fprintf(stderr, "Error reading file: `%s`\n", input);
+
+    exit(EXIT_FAILURE);
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char **argv) {
-  indent = 2;
-
-  char *program = pop(argc, argv);
-
-  printf("program name: %s\n", program);
-
-  if (!argc) {
-    usage(program);
-
-    exit(EXIT_FAILURE);
-  }
-
-  mode = pop(argc, argv);
-
-  if (!argc) {
-    usage(program);
-
-    exit(EXIT_FAILURE);
-  }
-
-  file = pop(argc, argv);
-
-  if (!load(file)) {
-    fprintf(stderr, "Error reading file: `%s`\n", file);
-
-    exit(EXIT_FAILURE);
-  }
-
   if (atexit(cleanup)) {
     fprintf(stderr, "atexit: Failed to register function: cleanup\n");
 
     exit(EXIT_FAILURE);
   }
 
-  if (!strcmp(mode, "add")) {
-    write_back = true;
+  indent = INDENT;
 
-    while ((line = bestlineWithHistory("trans >>> ", "trans"))) {
-      bool ret = parse(line, .indent = indent);
+  char *program = pop(argc, argv);
 
-      if (!ret) {
-        fprintf(stderr, "Couldn't parse: `%s`\n", line);
-      }
+  printf("program name: %s\n", program);
 
-      free(line);
-    }
-
-    printf("exit()\n");
+  if (!strcmp(program, "add")) {
+    add(argc, argv);
   }
 
-  else if (!strcmp(mode, "gen")) {
-    fprintf(stderr, "gen mode\n");
+  else if (!strcmp(program, "gen")) {
+    gen(argc, argv);
   }
 
-  else {
+  if (!argc) {
     usage(program);
 
     exit(EXIT_FAILURE);
   }
 
-  exit(EXIT_SUCCESS);
+  char *mode = pop(argc, argv);
+
+  if (!strcmp(mode, "add")) {
+    add(argc, argv);
+  }
+
+  else if (!strcmp(mode, "gen")) {
+    gen(argc, argv);
+  }
+
+  usage(program);
+
+  exit(EXIT_FAILURE);
 }
