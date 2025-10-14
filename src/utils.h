b95955a2d727d49578bc34c157f0503e7c787a1b
Bella Campbell <Bella.Campbell@tuta.io>
2025-10-14 16:52:47 +0100

    get rid of uninitialized memory warning

--- src/utils.h
+++ src/utils.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <stdlib.h>
+#include <string.h>
+
 // https://gist.github.com/rexim/b5b0c38f53157037923e7cdd77ce685d
-#define da_append(xs, x)                                                       \
-  do {                                                                         \
-    if ((xs)->count >= (xs)->capacity) {                                       \
-      if ((xs)->capacity == 0) (xs)->capacity = 256;                           \
-      else (xs)->capacity *= 2;                                                \
-      (xs)->items = realloc((xs)->items, (xs)->capacity*sizeof(*(xs)->items)); \
-    }                                                                          \
-    (xs)->items[(xs)->count++] = (x);                                          \
+#define da_append(xs, x)                                                                   \
+  do {                                                                                     \
+    if ((xs)->count >= (xs)->capacity) {                                                   \
+      size_t old_cap = (xs)->capacity;                                                     \
+      if ((xs)->capacity == 0) (xs)->capacity = 256;                                       \
+      else (xs)->capacity *= 2;                                                            \
+      (xs)->items = realloc((xs)->items, (xs)->capacity*sizeof(*(xs)->items));             \
+      memset((xs)->items + old_cap, 0, ((xs)->capacity - old_cap) * sizeof(*(xs)->items)); \
+    }                                                                                      \
+    (xs)->items[(xs)->count++] = (x);                                                      \
   } while (0)
 
 enum mode {
