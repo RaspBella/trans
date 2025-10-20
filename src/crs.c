98d727fc324aa049b64dae59c89cefdd9a2e1734
Bella Campbell <bella.campbell@tuta.io>
2025-10-20 08:37:27 +0100

    Add some pseudo stations

--- src/crs.c
+++ src/crs.c
@@ -3,6 +3,8 @@
 
 #include "crs.h"
 
+#define SEP "\n"
+
 #define CTOI(c) c - 'A'
 #define INDEX_DB(code) db[CTOI(code[0])][CTOI(code[1])][CTOI(code[2])]
 #define STATION(name, code) INDEX_DB(code) = name
